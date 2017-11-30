de8e1ad9f71a72845eb0002b37337f40c99b87f5
Brandon Wiley <brandon@blanu.net>
2017-11-29 22:57:31 -0600

    Initial Commit

---
+++ Auburn/Auburn.h
@@ -0,0 +1,19 @@
+//
+//  Auburn.h
+//  Auburn
+//
+//  Created by Brandon Wiley on 11/29/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Auburn.
+FOUNDATION_EXPORT double AuburnVersionNumber;
+
+//! Project version string for Auburn.
+FOUNDATION_EXPORT const unsigned char AuburnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auburn/PublicHeader.h>
+
+
