e7ce46cb11a9733cd5f45b2ccd144abb519d205c
Lita <looklita@gmail.com>
2018-02-20 15:44:26 -0600

    ZINCRBY Allows More Precise Incrementation

--- Sources/Auburn/RSortedSet.swift
+++ Sources/Auburn/RSortedSet.swift
@@ -243,7 +243,7 @@ public final class RSortedSet<LiteralType: Datable>: RBase, ExpressibleByArrayLi
         return (itemKey, Float(String(describing: result))!)
     }
     
-    public func incrementScore(ofField fieldKey: LiteralType, byIncrement increment: Int) -> Double?
+    public func incrementScore(ofField fieldKey: LiteralType, byIncrement increment: Double) -> Double?
     {
         guard let redis = Auburn.redis
         else
