9db8dd0e45204c62bfa5332258f8df43a32f8061
Lita <looklita@gmail.com>
2018-11-06 09:57:34 -0600

    Dependencies

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["Auburn"])
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Redshot.git", from: "0.3.8")
+        .package(url: "https://github.com/OperatorFoundation/Redshot.git", from: "0.3.11")
     ],
     targets: [
         .target(
